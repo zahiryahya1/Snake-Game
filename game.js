55b994cc91d91dd1194e17a780c91ff5b75e6754
zahir yahya <zahir_yahya@yahoo.com>
2018-01-08 16:23:22 -0800

    add movement with keypress

--- game.js
+++ game.js
@@ -13,3 +13,15 @@ function draw() {
 	s.show();
 
 }
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		s.dir(0, -1);
+	} else if (keyCode === DOWN_ARROW) {
+		s.dir(0, 1);
+	} else if (keyCode === LEFT_ARROW) {
+		s.dir(-1, 0);
+	} else if (keyCode === RIGHT_ARROW) {
+		s.dir(1, 0);
+	}
+}

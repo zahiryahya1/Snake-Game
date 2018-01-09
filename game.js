fd039a498603ab7ded972b80aecd3ecae1c00336
zahir yahya <zahir_yahya@yahoo.com>
2018-01-08 16:35:12 -0800

    add food to the game

--- game.js
+++ game.js
@@ -1,16 +1,31 @@
 var s;
 var scl = 20;
+var food;
 
 function setup() {
 	createCanvas(600, 600);
 	s = new Snake();
 	frameRate(10);
+	pickLocation();
+}
+
+function pickLocation() {
+	var cols = floor(width/scl);
+	var rows = floor(height/scl);
+	food = createVector(
+		floor(random(cols)), 
+		floor(random(rows) 
+	));
+	food.mult(scl);
 }
 
 function draw() {
 	background(51);
 	s.update();
 	s.show();
+
+	fill(255, 0, 100);
+	rect(food.x, food.y, scl, scl);
 
 }
 
